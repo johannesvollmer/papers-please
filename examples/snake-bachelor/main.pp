ef6e1843a5239fe49fcf15127a2cd0a3acaf2754
johannesvollmer <johannes.boenki@t-online.de>
2020-07-03 03:39:48 +0200

    prototype `.pp` syntax

---
+++ examples/snake-bachelor/main.pp
@@ -0,0 +1,38 @@
+cover `cover-v07-final-B-3-FINAL-asdasfasdaf.pdf`
+
+title-page
+  title `Evaluation of Snakes as Flight Attendants from a User Experience Standpoint`
+  purpose `Bachelor's Thesis`
+
+  author
+    name `Timothy Dinklerberg`
+    email `timothy-dinklerberg@ustcs.ucts`
+
+  examiners
+    `Prof. Dr. Kladdenhardt`
+    `Prof. Dr. Dr. Weißensteiner`
+
+abstract
+`Snakes are not as socially accepted as dogs or cats.
+In this study, we prove that snakes may be an
+adequate alternative to human flight attendants.`
+
+figures
+  folder `images`
+  backlink `true`
+
+  @snek `snake.png`
+  @len `snake-length-by-social-distance-diagram.svg`
+
+abbreviations
+  @`SINK` `Snakes In Natural Knots`
+  @`SOAP` `Snakes On A Plane`
+
+contents
+  folder `chapters`
+  @introduction `introduction.pp`
+  @analysis     `analysis.pp`
+  @evaluation   `evaluation.pp`
+  @discussion   `discussion.pp`
+
+references `references.pp`
---
+++ examples/snake-bachelor/references.pp
@@ -0,0 +1,15 @@
+add-waybackmachine-links `true`
+
+
+# sorting is set in the style template!
+
+
+@kouhia15 `10.1016/j.algal.2015.04.001`
+@hellaswag `10.18653/v1/p19-1472`
+
+@fb
+  url `facebook.com`
+  checked `3.7.2020`
+  owner `facebook inc.`
+
+# TODO: Images with a reference attached
